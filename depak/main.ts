deb63c7bcbda1cde976fc6775052509efed2c594
arisris <aris@mail.kliksob.com>
2023-02-05 20:32:36 +0700

    Mod

--- depak/main.ts
+++ depak/main.ts
@@ -1,9 +1,7 @@
 import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 
-
-
 const handleRequest = async (req: Request) => {
-  const { pathname } = new URL(req.url);
+  const url = new URL(req.url);
   await Promise.resolve(1);
   return new Response("Not Found", { status: 404 });
 };
